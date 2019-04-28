2bc3499289d00e5e2837acd19c33c33605b3815d
masao999 <masao99@t-net.ne.jp>
2019-04-28 20:13:03 +0900

    add: 各種ハンドラ追加

--- src/main/java/masao999/springbootsample/security/SecurityConfig.java
+++ src/main/java/masao999/springbootsample/security/SecurityConfig.java
@@ -1,20 +1,46 @@
 package masao999.springbootsample.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final AuthenticationSuccessHandler authenticationSuccessHandler;
+
+    private final AuthenticationFailureHandler authenticationFailureHandler;
+
+    private final AuthenticationEntryPoint authenticationEntryPoint;
+
+    @Autowired
+    public SecurityConfig(
+            AuthenticationSuccessHandler authenticationSuccessHandler,
+            AuthenticationFailureHandler authenticationFailureHandler,
+            AuthenticationEntryPoint authenticationEntryPoint) {
+        this.authenticationSuccessHandler = authenticationSuccessHandler;
+        this.authenticationFailureHandler = authenticationFailureHandler;
+        this.authenticationEntryPoint = authenticationEntryPoint;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers("/login").permitAll()
                 .anyRequest()
                 .authenticated();
 
+        http.exceptionHandling()
+                .authenticationEntryPoint(authenticationEntryPoint);
+
         http.formLogin()
-                .loginProcessingUrl("/login");
+                .loginProcessingUrl("/login").permitAll()
+                .successHandler(authenticationSuccessHandler)
+                .failureHandler(authenticationFailureHandler);
 
         http.logout()
                 .logoutUrl("/logout").permitAll()
