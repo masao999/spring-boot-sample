93d6eb3dd9abcdd8a03dab1bf78360cc9a9b0e5e
masao999 <masao99@t-net.ne.jp>
2019-04-29 05:30:12 +0900

    add: HelloServiceImplTest新規追加

---
+++ src/test/java/masao999/springbootsample/service/impl/HelloServiceImplTest.java
@@ -0,0 +1,47 @@
+package masao999.springbootsample.service.impl;
+
+import masao999.springbootsample.repository.impl.HelloRepositoryImpl;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.when;
+
+/**
+ * {@link HelloServiceImpl}のテストケース
+ */
+public class HelloServiceImplTest {
+
+    /**
+     * hello APIのService実装クラス
+     */
+    private HelloServiceImpl helloService;
+
+    /**
+     * hello APIのeRepository実装クラスのモック
+     */
+    @Mock
+    private HelloRepositoryImpl mockHelloRepository;
+
+    /**
+     * 事前処理
+     */
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        helloService = new HelloServiceImpl(mockHelloRepository);
+    }
+
+    /**
+     * {@link HelloServiceImpl#hello()}のテストケース
+     */
+    @Test
+    public void testHello() {
+        when(mockHelloRepository.hello()).thenReturn("hello");
+
+        assertThat(helloService.hello(), is("hello"));
+    }
+}
